0f12a04b73fc88f3259e2504fd112cdfb24dc97b
sashuttle <sashuttle25@outlook.com>
2024-06-14 20:28:33 -0500

    got rid of code that is not valid

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -1,13 +1,11 @@
 //FIXME: Fix this file
 import React, { useState, useEffect } from 'react';
 import {
-  Jumbotron,
   Container,
   Col,
   Form,
   Button,
   Card,
-  CardColumns
 } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
