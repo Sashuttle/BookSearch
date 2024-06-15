3509f0b52037a5a667e7acf8a47fe9ab9f04c7b1
sashuttle <sashuttle25@outlook.com>
2024-06-14 20:24:28 -0500

    realized switch is not valid, corrected code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,41 @@
 //FIXME: Create an Apollo Provider to make every request work with the Apollo server
-import './App.css';
-import { Outlet } from 'react-router-dom';
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { ApolloClient, ApolloProvider } from '@apollo/react-hooks';
+//import { Outlet } from 'react-router-dom';
+import SavedBooks from './pages/SavedBooks';
+import SearchBooks from './pages/SearchBooks';
 import Navbar from './components/Navbar';
+import './App.css';
+
+
+const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    });
+  },
+  uri: '/graphql',
+});
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
       <Navbar />
-      <Outlet />
-    </>
+      <Routes>
+        <Route exact path='/' component={SavedBooks} />
+        <Route exact path='/saved' component={SearchBooks} />
+        <Route render={() => <h1 className='display-2'>Wrong Page</h1>} />
+      </Routes>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
